7c51063560e1697dc566d1100f29768a481ae41b
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-27 13:40:38 +0100

    DOC: Switch to bizstyle theme

--- doc/conf.py
+++ doc/conf.py
@@ -119,6 +119,24 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'bizstyle'
+html_theme_options = {
+    #'nosidebar': True,
+    'body_min_width': 0,
+    #'body_max_width': None,
+    'navigation_with_keys': True,
+    #'rightsidebar': True,
+    #'maincolor': '#336699',
+}
+html_sidebars = {
+    '**': [
+        'globaltoc.html',
+        #'localtoc.html',
+        #'relations.html',
+        'sourcelink.html',
+        'searchbox.html',
+    ]
+}
 
 # -- Options for LaTeX output ---------------------------------------------
 
